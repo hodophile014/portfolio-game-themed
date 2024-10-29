001cc6f1af20ca6f4e71133ad2e6a3e88f84d958
Himanshi@07 <98021773+hodophile014@users.noreply.github.com>
2024-10-29 15:28:19 +0530

    Update Info.jsx

--- src/Context/Info.jsx
+++ src/Context/Info.jsx
@@ -5,7 +5,7 @@ export const Projects = [
         about: "it is a landing page created using reactjs and tailwindCSS",
         url: "https://github.com/hodophile014/ANIMEDEX-A-LANDING-PAGE-USING-REACTJS-",
 
-        image: "https://github.com/hodophile014/portfolio-game-themed/blob/main/src/Assests/Screenshot%202024-10-19%20174513.png"
+        image: "animedex.png"
          
     },
     {
@@ -13,14 +13,14 @@ export const Projects = [
         skills: ["React","Tailwind","React Hooks"],
         about: "it is whack a mole game created using tailwind and reactjs hooks",
         url: "https://github.com/hodophile014/WhackAMole",
-        image: "https://github.com/hodophile014/portfolio-game-themed/blob/main/src/Assests/Screenshot%202024-10-19%20174539.png"
+        image: "whack.png"
     },
     {   
         name: "ShoppingHub.com",
         skills: ["java","JSP","SERVLETS","SQL"],
         about: ["JSP & Servlets based ecommerce site build "],
         url: "https://github.com/hodophile014/ECOMMERCE-PLATFORM-USING-JSP-SQL-AND-SERVLETS",
-        image: "https://github.com/hodophile014/portfolio-game-themed/blob/main/src/Assests/shopping.png"
+        image: "shopping.png"
         
 
     },
@@ -29,7 +29,7 @@ export const Projects = [
         skills: ["REACTJS","TAILWINDCSS","REACT ROUTER"],
         about: ["A tom and jerry wiki website that tells about the tom and jerry and it's characters "],
         url: "https://vercel.com/hodophile014s-projects/tom-and-jerry-wiki",
-        image: "https://github.com/hodophile014/portfolio-game-themed/blob/main/src/Assests/tomandjerry.png"
+        image: "tomandjerry.png"
         
 
     },
